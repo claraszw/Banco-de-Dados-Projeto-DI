26a72948fa00ba409a990bbd975b09a1c67c320b
claraszw <clara_szw@hotmail.com>
2014-11-22 15:41:38 -0200

    mais views (odeio o postgres)

--- views.sql
+++ views.sql
@@ -1,20 +1,80 @@
 create view alunos_pos as
 (select *
- from alunos natural join posgrad)
+ from aluno natural join posgrad)
  
  create view alunos_doutorado as
  (select *
-  from alunos natural join grad
+  from aluno natural join posgrad
   where tipo = 'doutorado')
   
   create view alunos_mestrado as
  (select *
-  from alunos natural join grad
+  from aluno natural join posgrad
   where tipo = 'mestrado')
   
  create view monitores as
  (select *
-  from monitoria, alunos
+  from monitoria, aluno
   where aluno = matricula)
   
   
+ create view monitores_grad_nao_eng as
+ (
+	SELECT *
+	FROM aluno
+	WHERE matricula IN ( SELECT G.matricula
+                         FROM grad as G, monitoria as M
+                         WHERE G.matricula = M.aluno
+                         AND G.curso NOT LIKE '%Engenharia%'
+                         AND M.data_fim IS NULL)
+	ORDER BY nome
+ )
+  
+  
+ create view monitor_e_estagiario as
+ (
+	SELECT M.matricula, M.nome, E.laboratorio
+    FROM monitores as M left outer join estagiolab as E on M.aluno = E.aluno
+ )
+ 
+ create view posgrad_2013 as
+ (
+	SELECT COUNT (*)
+	FROM posgrad
+	WHERE PeriodoInicio like '2013.?'
+ )
+ 
+ create view cred_sergio as
+ (
+	SELECT SUM(numcred)
+	FROM projetoorientado INNER JOIN professor ON 	projetoorientado.matriculaprof = professor.matricula 
+	WHERE professor.nome = 'Sergio Lifschitz'
+ )
+ 
+ create view orient_e_coorient as
+ (
+	SELECT nome
+	FROM professor
+	WHERE matricula IN
+            (SELECT P.matricula
+            FROM professor as P, posgrad as PG
+            WHERE PG.coorientador=P.matricula)
+            INTERSECT
+            (SELECT P.matricula
+            FROM professor as P, posgrad as PG
+            WHERE PG.orientador=P.matricula)
+ )
+ 
+ create view monitores_progI as
+ (
+	SELECT aluno
+	FROM monitoria
+	WHERE codigo = 'INF1005'
+	EXCEPT(
+				SELECT aluno
+				FROM monitoria
+				WHERE data_fim IS NOT NULL
+            )
+ )
+ 
+ 
